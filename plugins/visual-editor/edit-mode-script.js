78ac3d4d6cbe35cb623a19b04d6f3b6a9bd4ac21
AceAbhishekBlockchain <abhishekawalkar@aceabhishek.com>
2025-06-06 21:24:59 +0530

    Update edit-mode-script.js

--- plugins/visual-editor/edit-mode-script.js
+++ plugins/visual-editor/edit-mode-script.js
@@ -153,9 +153,9 @@ async function handlePopupSave() {
     const result = await response.json();
     if (result.success) {
       const ALLOWED_PARENT_ORIGINS = [
-        'https://horizons.hostinger.com',
-        'https://horizons.hostinger.dev',
-        'https://horizons-frontend-local.hostinger.dev',
+        'https://aceabhishek.com',
+        'https://aceabhishek.dev',
+        'https://www.aceabhishek.dev',
       ];
 
       const parentOrigin = getParentOrigin();
